e9ff4c493a3eb2cd2567bee431c2bc140eb1fac2
Kevinvv77 <kevin_vanvliet@hotmail.com>
2022-09-10 19:08:56 +0200

    Adres domain

---
+++ p3/src/domain/Adres.java
@@ -0,0 +1,60 @@
+package domain;
+
+public class Adres {
+    private int id;
+    private String postcode;
+    private String huisnummer;
+    private String woonplaats;
+    private Reiziger reiziger;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getHuisnummer() {
+        return huisnummer;
+    }
+
+    public void setHuisnummer(String huisnummer) {
+        this.huisnummer = huisnummer;
+    }
+
+    public String getWoonplaats() {
+        return woonplaats;
+    }
+
+    public void setWoonplaats(String woonplaats) {
+        this.woonplaats = woonplaats;
+    }
+
+    public Reiziger getReiziger() {
+        return reiziger;
+    }
+
+    public void setReiziger(Reiziger reiziger) {
+        this.reiziger = reiziger;
+    }
+
+    @Override
+    public String toString() {
+        return "Adres{" +
+                "id=" + id +
+                ", postcode='" + postcode + '\'' +
+                ", huisnummer='" + huisnummer + '\'' +
+                ", woonplaats='" + woonplaats + '\'' +
+                ", reiziger=" + reiziger +
+                '}';
+    }
+}
